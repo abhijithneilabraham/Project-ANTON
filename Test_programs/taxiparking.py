3d97022b132d57f3cc41c04270e8ce4cb2b54da1
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-12-26 12:12:37 +0530

    Update Test_programs/taxiparking.py

--- Test_programs/taxiparking.py
+++ Test_programs/taxiparking.py
@@ -8,4 +8,10 @@ Created on Thu Dec 26 11:41:33 2019
 
 import gym
 env=gym.make("Taxi-v2").env
-env.render()
+i=0
+while i<5:
+    env.render()
+    env.reset()
+    print("action space {}".format(env.action_space))
+    print("observation space {}".format(env.observation_space))
+    i+=1
