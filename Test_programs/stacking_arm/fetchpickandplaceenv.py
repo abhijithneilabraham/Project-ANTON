98226a6b9d3c4a29bbe56f6f62b9635232b072ab
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2020-01-31 02:20:23 +0530

    necessary updates

--- Test_programs/stacking_arm/fetchpickandplaceenv.py
+++ Test_programs/stacking_arm/fetchpickandplaceenv.py
@@ -6,10 +6,10 @@ obs,reward,done,info=env.step(env.action_space.sample())
 reward=env.compute_reward(obs['achieved_goal'],obs['desired_goal'],info)
 print(obs)
 
-MAX_EPISODES = 900
+MAX_EPISODES = 100
 MAX_EP_STEPS = 200
-a_dim=13
-s_dim=9
+a_dim=2
+s_dim=2
 a_bound=[-1, 1]
 rl = DDPG(a_dim, s_dim, a_bound)
 def train():
@@ -20,11 +20,11 @@ def train():
         for j in range(MAX_EP_STEPS):
             # env.render()
 
-            a = rl.choose_action(s)
+            a = env.action_space.sample()
 
-            s_, r, done = env.step(env.action_space.sample())
+            s_, r, done,info = env.step(a)
 
-            rl.store_transition(s, a, r, s_)
+#            rl.store_transition(s, a, r, s_)
 
             ep_r += r
             if rl.memory_full:
@@ -37,3 +37,10 @@ def train():
                 break
     rl.save()
 train()
+s=env.reset()
+while True:
+    env.render()
+    a =env.action_space.sample()
+    s, r, done,info = env.step(a)
+    
+    
