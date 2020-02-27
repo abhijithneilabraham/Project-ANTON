429be60a20a7766c843da6f743b8b5b048b6112a
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2020-02-27 17:00:21 +0530

    Update Test_programs/mujocoenvcreator/examples/anton.py

---
+++ Test_programs/mujocoenvcreator/examples/anton.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 27 16:57:13 2020
+
+@author: abhijithneilabraham
+"""
+
+import numpy as np
+from mujoco_worldgen import Env, WorldParams, WorldBuilder, Floor, ObjFromXML
+
+
+def get_reward(sim):
+    object_xpos = sim.data.get_site_xpos("object")
+    target_xpos = sim.data.get_site_xpos("target")
+    ctrl = np.sum(np.square(sim.data.ctrl))
+    return -np.sum(np.square(object_xpos - target_xpos)) - 1e-3 * ctrl
+
+
+def get_sim(seed):
+    world_params = WorldParams(size=(10., 6., 4.5))
+    builder = WorldBuilder(world_params, seed)
+    floor = Floor()
+    builder.append(floor)
+    obj = ObjFromXML("particle_hinge")
+    floor.append(obj)
+    obj.mark("object")
+    floor.mark("target", (.5,   .5, 0.05))
+    return builder.get_sim()
+
+
+def make_env():
+    return Env(get_sim=get_sim, get_reward=get_reward, horizon=30)
